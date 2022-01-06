4ada72de3d061c0d739e65e3b942eeef0f6426b6
natalia-szymczyk <nataliaszymczyk17@gmail.com>
2022-01-06 17:31:10 +0100

    first attempt to heuristic evaluation

--- games-src/Players/NewPlayer/src/main/java/put/ai/games/newplayer/NewPlayer.java
+++ games-src/Players/NewPlayer/src/main/java/put/ai/games/newplayer/NewPlayer.java
@@ -14,11 +14,12 @@ import put.ai.games.game.Player;
 public class NewPlayer extends Player {
 
     private Random random = new Random(0xdeadbeef);
+    private int numberOfMove = 0;
 
 
     @Override
     public String getName() {
-        return "Natalia Szymczyk Jan Swiatek";
+        return "Natalia Szymczyk 145250 Jan Swiatek 145390";
     }
 
 
@@ -26,19 +27,67 @@ public class NewPlayer extends Player {
     public Move nextMove(Board b) {
         List<Move> moves = b.getMovesFor(getColor());
 
-        PrintWriter writer = null;
-        try {
-            writer = new PrintWriter("the-file-name.txt", "UTF-8");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+//        PrintWriter writer = null;
+//        try {
+//            writer = new PrintWriter(String.format("./logs/log%d.txt", numberOfMove++), "UTF-8");
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        }
+//        writer.println(moves.size());
+
+        int moveProfitability;
+        int bestProfitability = 0;
+        Move bestMove = moves.get(0);
+
+        for (Move move : moves){
+
+            moveProfitability = calculateProfitability(b, move);
+
+            if (moveProfitability > bestProfitability){
+                bestMove = move;
+                bestProfitability = moveProfitability;
+            }
+
+//            writer.println(move.toString() + " profitability: " + moveProfitability);
         }
-        writer.println("The first line");
-        writer.println("The second line");
-        writer.close();
 
+//        writer.close();
 
-        return moves.get(random.nextInt(moves.size()));
+        return bestMove;
+    }
+
+    public int calculateDifference(Board b){
+        int size = b.getSize();
+        int ourPawns = 0;
+        int enemiesPawns = 0;
+
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
+                if (b.getState(i, j) == Color.EMPTY){
+                    ;
+                }
+                else if (b.getState(i, j) == this.getColor()){
+                    ourPawns += 1;
+                }
+                else {
+                    enemiesPawns += 1;
+                }
+            }
+        }
+
+        return ourPawns - enemiesPawns;
+    }
+
+    public int calculateDifferenceAfterMove(Board b, Move m){
+        b.doMove(m);
+        int difference = calculateDifference(b);
+        b.undoMove(m);
+        return difference;
+    }
+
+    public int calculateProfitability(Board b, Move m){
+        return calculateDifferenceAfterMove(b, m) - calculateDifference(b);
     }
 }
