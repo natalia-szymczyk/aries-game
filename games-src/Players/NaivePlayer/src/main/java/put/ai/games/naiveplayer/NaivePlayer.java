85c8b196bd12e77083a6dd51258f26149a53f51d
natalia-szymczyk <nataliaszymczyk17@gmail.com>
2022-01-06 15:37:09 +0100

    init

---
+++ games-src/Players/NaivePlayer/src/main/java/put/ai/games/naiveplayer/NaivePlayer.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package put.ai.games.naiveplayer;
+
+import java.io.*;
+import java.util.List;
+import java.util.Random;
+import put.ai.games.game.Board;
+import put.ai.games.game.Move;
+import put.ai.games.game.Player;
+
+public class NaivePlayer extends Player {
+
+    private Random random = new Random(0xdeadbeef);
+
+
+    @Override
+    public String getName() {
+        return "Gracz Naiwny 84868";
+    }
+
+
+    @Override
+    public Move nextMove(Board b) {
+        List<Move> moves = b.getMovesFor(getColor());
+
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("the-file-name.txt", "UTF-8");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        writer.println("The first line");
+        writer.println("The second line");
+        writer.close();
+
+
+        return moves.get(random.nextInt(moves.size()));
+    }
+}
